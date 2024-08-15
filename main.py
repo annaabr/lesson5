afa4de94f4bbe91bc025cbb9fe09efb4f41e64e6
annaabr <annaabramyan2013@gmail.com>
2024-08-15 23:36:44 +0300

    new

--- main.py
+++ main.py
@@ -6,23 +6,35 @@ class Warrior():
         self.hair_color = hair_color
 
     def sleep(self):
-        print(f"{self.name} is sleeping..")
+        print(f"{self.name} спит..")
         self.energy += 2
 
     def eat(self):
-        print(f"{self.name} sat down to eat..")
+        print(f"{self.name} кушает..")
 
     def hit(self):
-        print(f"{self.name} shits someone..")
+        print(f"{self.name} бьет кого-то..")
         self.energy -= 6
 
     def walk(self):
-        print(f"{self.name} is walking..")
+        print(f"{self.name} гуляет..")
 
     def info(self):
-        print(f"Имя воина -{self.name}}")
-        print(f"Цвет волос воина -{self.hair_color}}")
-        print(f"Сила воина -{self.power}}")
-        print(f"Энергия воина -{self.energy}}")
+        print(f"Имя воина - {self.name}")
+        print(f"Цвет волос воина - {self.hair_color}")
+        print(f"Сила воина - {self.power}")
+        print(f"Энергия воина - {self.energy}")
 
+war1 = Warrior("Степа", 76, 54,"коричневый")
+print(war1.name)
 
+print(war1.energy)
+war1.sleep()
+print(war1.energy)
+war1.eat()
+print(war1.energy)
+war1.walk()
+print(war1.energy)
+war1.hit()
+print(war1.energy)
+war1.info()
